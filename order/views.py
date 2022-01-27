ea18dc6aaace7a09bb4eda7573cddc8074639e76
kirank93611 <kirank93611@gmail.com>
2022-01-28 00:30:08 +0530

    new

--- order/views.py
+++ order/views.py
@@ -14,8 +14,8 @@ from .models import Order, OrderItem
 from .serializers import OrderSerializer, MyOrderSerializer
 
 @api_view(['POST'])
-@authentication_classes([authentication.TokenAuthentication])
-@permission_classes([permissions.IsAuthenticated])
+# @authentication_classes([authentication.TokenAuthentication])
+# @permission_classes([permissions.IsAuthenticated])
 def checkout(request):
     serializer = OrderSerializer(data=request.data)
 
